ae7e89209112f31d8a5698cf52872d24f1fc41d8
Moffo Sergeo <sergeo.selagsa@cm.maviance.com>
2024-07-03 19:56:59 +0100

    Deuxième réécriture de chebyshev_distance

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 def chebyshev_distance(p1, p2):
     """Docstring..."""
-    distances = [abs(x - y) for x, y in zip(p1, p2)]
-    return max(distances)
+    return max(map(lambda x_y: abs(x_y[0] - x_y[1]), zip(p1, p2)))
 
 if __name__ == "__main__":
     point1 = (1, 2, 3)
